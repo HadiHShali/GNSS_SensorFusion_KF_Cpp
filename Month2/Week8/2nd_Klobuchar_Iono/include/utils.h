14ab1d8947ebaf5a8da7283486cd1d90caaa3581
HadiHShali <hadi.heydarizadeh@gmail.com>
2026-07-01 16:53:59 -0500

    Month2 Week8 Day4: Full atmospheric corrections integrated

--- Month2/Week8/2nd_Klobuchar_Iono/include/utils.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <vector>
-#include <string>
-#include "gps_ls_solver.h"
-
-std::vector<Observation> loadObservations(const std::string& filename);
-
-void ecefToLla(double X, double Y, double Z, double& lat, double& lon, double& h);
